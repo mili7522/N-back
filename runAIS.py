e7621880eae3b1eb7e1151ac154edfd6e14947f3
Mike <mili7522@uni.sydney.edu.au>
2019-05-21 00:48:29 +1000

    Improved AIS code with integrated preprocessing

--- runAIS.py
+++ runAIS.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 history_lengths = range(1, 6)
 delays = range(1, 2)
 
-calc = utils.startCalc()
+calc = utils.startCalc(measure = 'ais', estimator = 'ksg')
 
 def plotAISAcrossParams(ais_values):
     plt.imshow(ais_values, extent = (0.5, len(delays)+0.5, len(history_lengths)+0.5, 0.5))
@@ -18,17 +18,30 @@ def plotAISAcrossParams(ais_values):
     plt.savefig('AIS_parameters.png', dpi = 200, bbox_inches='tight')
     plt.show()
 
-def computeAIS(k, tau, data, calc = None, compute_local = False, compute_p = False):
-    if calc is None:
-        calcClass = jpype.JPackage("infodynamics.measures.continuous.kraskov").ActiveInfoStorageCalculatorKraskov
-        calc = calcClass()
+def computeAIS(k, tau, acl, data, calc, compute_local = False, compute_p = False, number_of_surrogates = 1000):
+    """
+    Performs a calculation of Active Information Storage using JIDT
+
+    Arguments:
+        k -- History length parameter
+        tau -- Delay parameter
+        acl -- Auto correlation length, used to set the dynamic correlation exclusion property
+        data -- 1D Numpy array
+        calc -- The JIDT calculator
+        compute_local -- If True, the local AIS is returned instead of the expectation
+        compute_p  -- If True, the p value of the average AIS
+        number_of_surrogates
     
-    # calc.setProperty("k_HISTORY", str(k))
-    # calc.setProperty("tau", str(tau))
-    calc.initialise(k, tau)
+    Returns:
+        AIS -- Either locals or average
+        p -- p value. Returned if compute_p is true
+    """
+    calc.setProperty("k_HISTORY", str(k))
+    calc.setProperty("tau", str(tau))
+    calc.setProperty('DYN_CORR_EXCL', str(acl))
     calc.setObservations(data)
     if compute_p:
-        measDist = calc.computeSignificance(1000)
+        measDist = calc.computeSignificance(number_of_surrogates)
         # print("Surrogate Mean:", measDist.getMeanOfDistribution())
         # print("Surrogate Std:", measDist.getStdOfDistribution())
         p = measDist.pValue
@@ -39,51 +52,86 @@ def computeAIS(k, tau, data, calc = None, compute_local = False, compute_p = Fal
     else:
         return calc.computeAverageLocalOfObservations(), p  # KSG Estimator is already bias adjusted
 
-def getLocalsForRegion(region_idx, df, print_max_idx = True, compute_p = False):
-    data = df.iloc[region_idx].values
-    data = utils.preprocess(data)
-    
-    ais_values = np.zeros((len(history_lengths), len(delays)))
 
+def getLocalsForRegion(data, region_idx = None, print_max_idx = True, compute_p = False):
+    """
+    Calculates the local AIS for a particular region, after first determining the appropriate values for the history length and delay parameters
+
+    Arguments:
+        data -- Numpy array. Either a 1d array containing the values for a particular region over time, or a 2d array of shape (region, time)
+                Preprocessing should have already been performed
+        region_idx -- None, or the index of the region under consideration, as an Int
+        print_max_idx -- If True, prints the maximum value AIS value and the index of the corresponding parameters in (history_lengths, delays)
+        compute_p -- If True, computes the p value of the returned AIS
+    """
+    assert data.ndim == 1 or region_idx is not None
+    if region_idx is not None:
+        data = data[region_idx]
+    
+    acl = utils.acl(data)
+
+    ais_values = np.zeros((len(history_lengths), len(delays)))
+    
     for i, history_length in enumerate(history_lengths):
         for j, delay in enumerate(delays):                   
-            ais_values[i, j], _ = computeAIS(history_length, delay, data, calc, compute_local=False)
+            ais_values[i, j], _ = computeAIS(history_length, delay, acl, data, calc, compute_local = False)
 
     max_idx = utils.getMaxIdx2D(ais_values, print_ = print_max_idx)
 
-    result, p = computeAIS(history_lengths[max_idx[0]], delays[max_idx[1]], data, calc, compute_local=True, compute_p = compute_p)
+    result, p = computeAIS(history_lengths[max_idx[0]], delays[max_idx[1]], acl, data, calc, compute_local = True, compute_p = compute_p)
     return result, ais_values, max_idx, p
 
-def getLocalsForAllRegions(df, print_max_idx = True, parameters = None, compute_p = False):
-    regions, timepoints = (333, 405)
+def getLocalsForAllRegions(data, print_max_idx = True, parameters = None, compute_p = False):
+    """
+    Calculates the local AIS for all regions, by calling getLocalsForRegion
+
+    Arguments:
+        data -- Numpy array of shape (region, time). Preprocessing should have already been performed
+    """
+    regions, timepoints = data.shape
     results = np.zeros((regions, timepoints))
     all_max_idx = np.zeros((regions, 2))
     p_values = np.zeros(regions)
+    acls = None
     for region in range(regions):
         if parameters is None:
-            results[region], _, max_idx, p_values[region] = getLocalsForRegion(region, df, print_max_idx, compute_p = compute_p)
+            results[region], _, max_idx, p_values[region] = getLocalsForRegion(data, region, print_max_idx, compute_p = compute_p)
             all_max_idx[region] = np.array(max_idx)
         else:
             p = parameters[region]
-            data = df.iloc[region].values
-            data = utils.preprocess(data)
-            results[region], p_values[region] = computeAIS(history_lengths[p[0]], delays[p[1]], data, calc, compute_local=True, compute_p = compute_p)
+            if acls is None:
+                acls = utils.acl(data)
+            results[region], p_values[region] = computeAIS(history_lengths[p[0]], delays[p[1]], acls[region], data[region], calc, compute_local = True, compute_p = compute_p)
     return results, all_max_idx, p_values
 
-def getAverageParametersAcrossPatients():
-    files = utils.getAllFiles()
-    regions = 333
+def getAverageParametersAcrossPatients(path, extension, **preprocessing_params):
+    """
+    Finds the parameters for history length and delay corresponding to the maximum average AIS over the population
+
+    Arguments:
+        path -- 
+        extension -- 
+        preprocessing_params -- Includes sampling_rate / sampling_interval, apply_global_mean_removal, trim_start, trim_end
+    """
+    files = utils.getAllFiles(path, extension)
+    ais_values = {}
+    for k, file in enumerate(files):
+        df = utils.loadData(file)
+        data = utils.preprocess(df, **preprocessing_params)
+        regions = data.shape[0]
+        acls = None
+        for region in range(regions):
+            if region not in ais_values:
+                ais_values[region] = np.zeros((len(files), len(history_lengths), len(delays)))
+            if acls is None:
+                acls = utils.acl(data)
+            for i, history_length in enumerate(history_lengths):
+                for j, delay in enumerate(delays):
+                    ais_values[region][k, i, j], _ = computeAIS(history_length, delay, acls[region], data[region], calc, compute_local = False)
+
     all_max_idx = np.zeros((regions, 2))
     for region in range(regions):
-        ais_values = np.zeros((len(files), len(history_lengths), len(delays)))
-        for k, file in enumerate(files):
-            df = utils.loadData(file)
-            data = df.iloc[region].values
-            data = utils.preprocess(data)
-            for i, history_length in enumerate(history_lengths):
-                for j, delay in enumerate(delays):           
-                    ais_values[k, i, j], _ = computeAIS(history_length, delay, data, calc, compute_local=False)
-        mean_ais_values = ais_values.mean(axis = 0)
+        mean_ais_values = ais_values[region].mean(axis = 0)
         max_idx = utils.getMaxIdx2D(mean_ais_values)
         all_max_idx[region] = np.array(max_idx)
     return all_max_idx
@@ -91,38 +139,63 @@ def getAverageParametersAcrossPatients():
 
 
 if __name__ == "__main__":
-    # filename = 100307
-    # region_idx = 0
-    # df = utils.loadData(filename = filename)
-    # result, ais_values, max_idx, p_value = getLocalsForRegion(region_idx = region_idx, df = df, compute_p = True)
-    # plotAISAcrossParams(ais_values)
-    # plt.plot(result); plt.xlabel('Time'); plt.ylabel('AIS')
-    # plt.title("AIS Kraskov: {}[{}]\nHistory = {}, Delay = {}".format(filename, region_idx, history_lengths[max_idx[0]], delays[max_idx[1]]))
+    def test_for_one_region():
+        filename = '100307.tsv'
+        path = '../Data'
+        region_idx = 0
+        df = utils.loadData(filename, path)
+        data = utils.preprocess(df, sampling_rate = 1.3, apply_global_mean_removal = True, trim_start = 50, trim_end = 25)
+        result, ais_values, max_idx, p_value = getLocalsForRegion(data, region_idx = region_idx, compute_p = False)
+        plotAISAcrossParams(ais_values)
+        plt.plot(result); plt.xlabel('Time'); plt.ylabel('AIS')
+        plt.title("AIS Kraskov: {}[{}]\nHistory = {}, Delay = {}".format(filename, region_idx, history_lengths[max_idx[0]], delays[max_idx[1]]))
 
-
-    # for i, file in enumerate(utils.getAllFiles()):
     i = int(sys.argv[1])
-    files = utils.getAllFiles()
-    file = files[i]
-    os.makedirs("Results/AIS Local - Individual Parameters/idx", exist_ok = True)
-    os.makedirs("Results/AIS Local - Individual Parameters/p_values", exist_ok = True)
-    os.makedirs("Results/AIS Local - Population Parameters/p_values", exist_ok = True)
 
-    # if os.path.exists('Results/AIS Local - Individual Parameters/{}_AIS.csv'.format(utils.basename(file))):
-    #     continue
-    print("Processing", i, ":", file)
-    # if os.path.exists('Results/AIS Local - Individual Parameters/p_values/{}.csv'.format(utils.basename(file))): exit()
-    df = utils.loadData(file)
-    results, all_max_idx, p_values = getLocalsForAllRegions(df, print_max_idx = False, compute_p = True)
-    # pd.DataFrame(results).to_csv('Results/AIS Local - Individual Parameters/{}_AIS.csv'.format(utils.basename(file)), index = None, header = None)
-    # pd.DataFrame(all_max_idx.astype(int)).to_csv('Results/AIS Local - Individual Parameters/idx/{}.csv'.format(utils.basename(file)), index = None, header = None)
-    # pd.DataFrame(p_values).to_csv('Results/AIS Local - Individual Parameters/p_values/{}.csv'.format(utils.basename(file)), index = None, header = None)
-    # utils.plotHeatmap(results, divergent = True)
+    def run_individual_parameters(i, path, extension, save_folder, GRP = False, **preprocessing_params):
+        """
+        Arguments:
+            GRP -- True if processing the GRP data
+        """
+        files = utils.getAllFiles(path, extension)
+        if GRP:
+            file = files[0]
+        else:
+            file = files[i]
+        os.makedirs("Results/{}/AIS/idx".format(save_folder), exist_ok = True)
+        os.makedirs("Results/{}/AIS/p_values".format(save_folder), exist_ok = True)
+        
+        print("Processing", i, ":", file)
+        if os.path.exists('Results/{}/AIS/p_values/{}.csv'.format(save_folder, utils.basename(file))):
+            exit()
+        if GRP:
+            df = utils.loadData(file, subject_id = i)
+        else:
+            df = utils.loadData(file)
+        data = utils.preprocess(df, **preprocessing_params)
+        results, all_max_idx, p_values = getLocalsForAllRegions(data, print_max_idx = False, compute_p = True)
+        # Add back the trimmed sections
+        padding = ((0,0), (preprocessing_params.get('trim_start', 0), preprocessing_params.get('trim_end', 0)))
+        results = np.pad(results, padding, mode = 'constant', constant_values = 0)
 
+        if GRP:
+            file = '{:02}'.format(i)    # Save the results by the subjects number
+        pd.DataFrame(results).to_csv('Results/{}/AIS/{}_AIS.csv'.format(save_folder, utils.basename(file)), index = None, header = None)
+        pd.DataFrame(all_max_idx.astype(int)).to_csv('Results/{}/AIS/idx/{}.csv'.format(save_folder, utils.basename(file)), index = None, header = None)
+        pd.DataFrame(p_values).to_csv('Results/{}/AIS/p_values/{}.csv'.format(save_folder, utils.basename(file)), index = None, header = None)
+        utils.plotHeatmap(results, divergent = True)
+
+    # HCP
+    run_individual_parameters(i, path = 'Data/HCP', extension = '.tsv', save_folder = 'HCP',
+                                sampling_rate = 1.3, apply_global_mean_removal = True, trim_start = 50, trim_end = 25)
+
+
+    ### Population parameters
+    # os.makedirs("Results/AIS Local - Population Parameters/p_values", exist_ok = True)
 #    if os.path.exists('Results/Population all_max_idx.csv'):
 #        all_max_idx = pd.read_csv('Results/Population all_max_idx.csv', header = None).values
 #    else:
-#        all_max_idx = getAverageParametersAcrossPatients()
+#        all_max_idx = getAverageParametersAcrossPatients(path = '../Data', extension = '.tsv', sampling_rate = 1.3, trim_start = 50, trim_end = 25)
 #        pd.DataFrame(all_max_idx.astype(int)).to_csv('Results/Population all_max_idx.csv', index = None, header = None)
 #    # for i, file in enumerate(utils.getAllFiles()):
 #    # if os.path.exists('Results/AIS Local - Population Parameters/{}_AIS.csv'.format(file.split('.')[0])):
--- utils.py
+++ utils.py
@@ -1,62 +1,198 @@
-import jpype
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.signal import detrend
 import os
+import h5py
+import jpype
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy.io import loadmat
+from scipy.signal import butter, detrend, filtfilt
+from scipy.stats import zscore
 
-def loadData(filename, get_params = False):
-    '''
-    Output is a dataframe of 333 regions vs 405 time points
-    '''
-    if type(filename) is int:
-        filename = str(filename)
-    if not filename.endswith('.tsv'):
-        filename = filename + '.tsv'
-    if not filename.startswith('Data'):
-        filename = 'Data/' + filename
-    df = pd.read_csv(filename, sep = '\t', index_col = 0)
+
+def loadData(filename, path = None, get_params = False, param_path = None, subject_id = None):
+    """
+    Loads the data (and the corresponding history length and delay if get_params is True)
+
+    Arguments:
+        filename -- Name (and path) of the data file to be loaded
+        path -- Location of the file, or None (if the filename argument already contains the path)
+        get_params -- If True, load the history length and delay corresponding to the file (found by maximising AIS)
+        param_path
+        subject_id -- Used for the GRP data. Selects which subject's data to load
+
+    Output:
+        df -- Dataframe of regions of interest (ROI) vs time points
+        params_df
+    """
+    if path is not None:
+        filename = os.path.join(path, filename)
+    if filename.endswith('.tsv'):  # The HCP data is made of 493 tsv files of 333 regions (rows) vs 405 time points (columns)
+        df = pd.read_csv(filename, sep = '\t', index_col = 0)
+    if filename.endswith('.csv'):  # The ATX data is made of 38 csv files (19 subjects with two files each) of 375 regions (rows) vs 286 time points (columns)
+        df = pd.read_csv(filename, header = None, index_col = None)
+    if filename.endswith('.mat'):  # DONE data or GRP data
+        if subject_id is None:
+            # The DONE data is made up of 28 mat files of 375 regions (matlab rows) vs 186 time points (matlab columns)
+            data = h5py.File(filename, 'r')
+            data = data['ts_matrix']  # Gives a HDF5 dataset. Can also get the array using data.value
+            df = pd.DataFrame(np.array(data).T)  # Need to transpose it since matlab uses column-major order and numpy uses row-major order
+        else:
+            # The GRP data is organised as 375 regions x 1940 time points x 100 subjects
+            data = loadmat(filename)
+            data = data['data_grp_full']
+            data = data[:,:,subject_id]
+            df = pd.DataFrame(data)
     if get_params:
-        param_path = 'Results/AIS Local - Individual Parameters/idx/{}.csv'.format(basename(filename))
-        param_df = pd.read_csv(param_path, header = None)
+        assert param_path is not None, "Need to provide a path where parameters are stored"
+        # eg param_path = Results/AIS Local - Individual Parameters/idx/
+        param_file = os.path.join(param_path, '{}.csv'.format(basename(filename)))
+        param_df = pd.read_csv(param_file, header = None)
         return df, param_df
     return df
 
 def getAllFiles(path = 'Data', extension = '.tsv'):
+    """
+    Returns a list of files in a particular folder that ends with a particular extension
+    """
     files = []
     for f in os.listdir(path):
         if f.endswith(extension):
             files.append(os.path.join(path, f))
     return sorted(files)
 
-def plotHeatmap(df, divergent = False):
+def plotHeatmap(df, divergent = False, divergent_cmap = 'RdBu', vmax = None, vmin = None):
     if divergent:
-        vmax = np.abs(df).max()
-        im = plt.imshow(df, cmap = 'RdBu', vmax = vmax, vmin = -vmax)
+        vmax = vmax or np.abs(df).max()
+        vmin = vmin or -vmax
+        im = plt.imshow(df, aspect = 'auto', cmap = divergent_cmap, vmax = vmax, vmin = vmin)
     else:
-        im = plt.imshow(df)
+        im = plt.imshow(df, aspect = 'auto')
     plt.xlabel('Time')
     plt.ylabel('Region')
-    plt.colorbar(im,fraction=0.046, pad=0.04)
+    plt.colorbar(im, fraction=0.046, pad=0.04)
     plt.show()
 
-def plotTimeseries(df, idx = 0):
-    if isinstance(df, np.ndarray):
-        y = pd.DataFrame(df)
+def plotTimeseries(data, region_idx = None):
+    """
+    Arguments:
+        data -- Numpy array of 1D or 2D or pandas DataFrame. If 2D, it is in the shape (regions, time)
+        region_idx -- The index of the region to plot, as an Int, or a list of indexs
+    """
+    assert data.ndim == 1 or region_idx is not None
+    if isinstance(data, pd.DataFrame):
+        y = data.iloc[region_idx].values
+    elif data.ndim == 2:
+        y = data[region_idx]
     else:
-        y = df.iloc[idx].T
-    x = range(0, len(y), 50)
-    ax = y.plot()
-    ax.set_xticks(x)
-    ax.set_xticklabels(x)
+        y = data
+    if y.ndim == 2:  # This is the case if region_idx is a list 
+        y = y.T
+    plt.plot(y)
+    plt.xlim([0, len(y)])
     plt.xlabel('Time')
+    if y.ndim == 2:
+        plt.legend(labels = region_idx)
     plt.show()
 
-def preprocess(array):
-    x = detrend(array)
-    return x / np.std(x)
+def preprocess(data, sampling_interval = None, sampling_rate = None, apply_global_mean_removal = True, trim_start = 0, trim_end = 0, **filter_params):
+    """
+    Applies preprocessing to the fMRI data, following a pipeline that will make it suitable for AIS / TE analysis
+
+    Arguments:
+        data -- Numpy array or pandas DataFrame of shape (regions, time)
+        sampling_interval -- None, or the interval between samples in seconds
+        sampling_rate -- None, or the number of samples per second
+        apply_global_mean_removal -- If True, global mean removal will be applied to the data
+        trim_start -- Number of timesteps to cut off the start of the data to counteract the effect of filtering
+        trim_end -- Number of timesteps to cut off at the end of the data to counteract the effect of filtering
+        filter_params -- Other filter parameters (fcuthigh and order)
+
+    Output:
+        data -- Numpy array after preprocessing: z-scoring, detrending, high pass filtering and applying global mean removal.
+                Some starting and ending timepoints may be trimmed
+    """
+    if isinstance(data, pd.DataFrame):
+        data = data.values
+    data = zscore(data, axis = 1, ddof = 1)
+    data = detrend(data, axis = 1)
+    data = high_pass_filter(data, sampling_interval, sampling_rate, **filter_params)
+    if apply_global_mean_removal:
+        data = data - np.mean(data, axis = 0, keepdims = True)
+    # Trim start and end
+    data = data[:, trim_start:]
+    if trim_end > 0:
+        data = data[:, :-trim_end]
+    return data
+
+def high_pass_filter(data, sampling_interval = None, sampling_rate = None, fcuthigh = 0.01, order = 3):
+    """
+    Returns the data after applying a high pass butterworth filter using the filtfilt function
+
+    Arguments:
+        data -- Numpy array in the shape (ROI, time)
+        sampling_interval -- Seconds per sample
+        sampling_rate -- Samples per second, eg. 1.3 samples per second for HCP data
+        fcuthigh -- Cutoff frequency (Hz)
+        order -- Order of the filter
+
+    Output:
+        data -- Numpy array containing the data after filtering is performed
+    """
+    # Setting up the filter
+    assert sampling_interval is not None or sampling_rate is not None
+    sampling_rate = sampling_rate or 1 / sampling_interval
+    Wn = fcuthigh / ( 1/2 * sampling_rate )
+    [b, a] = butter( order, Wn, 'high')
+
+    # Applying the filter
+    data = filtfilt( b, a, data, axis = 1, padtype = 'odd', padlen = 3 * ( max(len(a),len(b)) - 1) )
+    return data
+
+def acf(data, axis = 1):
+    """
+    Calculates the autocorrelation coefficients
+
+    Arguments:
+        data -- Numpy array of 1d or 2d
+        axis -- The number of the axis corresponding to the time dimension
+    
+    Outputs:
+        acf_coeffs -- Numpy array of 1d or 2d. If it's 2d, it is always returned in the shape (lag, ROI)
+    """
+    data = np.atleast_2d(data)
+    n = data.shape[axis]
+    mean = np.mean(data, axis = axis, keepdims = True)
+    var = np.sum((data - mean) ** 2, axis = axis)  # Unscaled variance
+    def r(h):
+        if axis == 0:
+            acf_lag = ((data[:n - h] - mean) * (data[h:] - mean)).sum(axis = axis) / var
+        elif axis == 1:
+            acf_lag = ((data[:,:n - h] - mean) * (data[:,h:] - mean)).sum(axis = axis) / var
+        return acf_lag
+    x = np.arange(n)  # Avoiding lag 0 calculation
+    acf_coeffs = np.array(list(map(r, x)))
+    return acf_coeffs
+
+def acl(data, axis = 1):
+    """
+    Calculates the autocorrelation length as expected by the dynamic correlation exclusion parameter in JIDT
+    """
+    acf_coeffs = acf(data, axis)
+    auto_correlation_lengths = np.ceil( 2 * np.sum( acf_coeffs ** 2, axis = 0) ) - 2  # Always sum over the 0th axis
+    auto_correlation_lengths = auto_correlation_lengths.astype(int)
+    return np.squeeze(auto_correlation_lengths)
 
 def getMaxIdx2D(array_2d, print_ = True):
+    """
+    Finds the (2D) index of the maximum value in a 2D array
+
+    Arguments:
+        array_2d -- 2D Numpy array
+        print_ -- If True, the maximum value and index is printed
+    
+    Returns:
+        max_idx -- A tuple of (row, col), that gives the location of the maximum value in the array
+    """
     a_len, b_len = array_2d.shape
     max_val = array_2d.max()
     max_idx = array_2d.argmax()
@@ -66,18 +202,23 @@ def getMaxIdx2D(array_2d, print_ = True):
     return max_idx
 
 def startCalc(measure = 'ais', estimator = 'ksg'):
+    """
+    Start the JIDT calculator
+    """
     if not jpype.isJVMStarted():
         if os.path.exists("../noradrenaline/bin/infodynamics.jar"):
             jarLocation = "../noradrenaline/bin/infodynamics.jar"
         else:
-            jarLocation = "../../Other Units/1. CSYS5030 - Self Organisation and Criticality/JIDT Toolkit/infodynamics.jar"
+            jarLocation = "/home/mike/Downloads/JIDT/infodynamics.jar"
         # Start the JVM (add the "-Xmx" option with say 1024M if you get crashes due to not enough memory space)
         jpype.startJVM(jpype.getDefaultJVMPath(), "-Xmx2048M", "-ea", "-Djava.class.path=" + jarLocation)
 
     if measure.lower() == 'ais':
-        calcClass = jpype.JPackage("infodynamics.measures.continuous.kraskov").ActiveInfoStorageCalculatorKraskov
+        if estimator.lower() == 'ksg':
+            calcClass = jpype.JPackage("infodynamics.measures.continuous.kraskov").ActiveInfoStorageCalculatorKraskov
     elif measure.lower() == 'te':
-        calcClass = jpype.JPackage("infodynamics.measures.continuous.kraskov").TransferEntropyCalculatorKraskov
+        if estimator.lower() == 'ksg':
+            calcClass = jpype.JPackage("infodynamics.measures.continuous.kraskov").TransferEntropyCalculatorKraskov
     calc = calcClass()
     return calc
 
@@ -100,4 +241,18 @@ def basename(file):
     return os.path.splitext(os.path.basename(file))[0]
 
 if __name__ == '__main__':
-    df = loadData(100307)
+    df_HCP = loadData('100307.tsv', path = '../Data')
+    def checkPreprocessing():
+        # Checking comparisons with matlab preprocessing
+        from scipy.io import loadmat
+        comparison_data = loadmat('../Preprocessing_steps_100307.mat')
+        data = preprocess(df_HCP.values, sampling_rate = 1.3)
+        print("Close after mean removal?", np.allclose(data.T, comparison_data['data_meanremoval']))
+        print("Close acl after filtering?", np.allclose(acl(data, axis = 1), comparison_data['acl_meanremoval'].ravel()))
+    checkPreprocessing()
+    # Load ATX data
+    df_ATX = loadData('STX0001-01_results.csv', path = '/media/mike/Files/Data and Results/N-back/Data/ATX_data')
+    # Load DONE data
+    df_DONE = loadData('R01A.mat', path = '/media/mike/Files/Data and Results/N-back/Data/DONE_data')
+    # Load GRP data
+    df_GRP = loadData('data_grp_full.mat', path = '/media/mike/Files/Data and Results/N-back/Data/data_grp_full', subject_id = 0)
